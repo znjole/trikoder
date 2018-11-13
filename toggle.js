bb097054ef64657629bb9b4bf84f29ce2a9c9a44
znjole <44868333+znjole@users.noreply.github.com>
2018-11-13 19:28:59 +0100

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,8 @@
+function openNav() {
+    var x = document.getElementById("mySidenav");
+    if (x.style.display === "none") {
+        x.style.display = "flex";
+    } else {
+        x.style.display = "none";
+	}
+}
