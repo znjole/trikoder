6ce442b8480dfe91de5dfd6373725509421feac0
znjole <44868333+znjole@users.noreply.github.com>
2018-11-19 19:06:04 +0000

    Delete toggle.js

--- toggle.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    var x = document.getElementById("mySidenav");
-    if (x.style.display === "none") {
-        x.style.display = "flex";
-    } else {
-        x.style.display = "none";
-	}
-}
